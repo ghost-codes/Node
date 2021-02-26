f297bae661327c3eed99eccd21cb817bb8a04e1d
hopedorkenoo <compounddork@gmail.com>
2021-02-26 15:29:02 +0000

    Javascript: Rest and Spread Operators

--- javaScript/arrays.js
+++ javaScript/arrays.js
@@ -6,3 +6,16 @@ for (let hobby of hobbiesArray) {
 
 console.log(hobbiesArray.map(hobby => "Hobby: " + hobby));
 console.log(hobbiesArray);
+
+
+// ... is the spread operator
+const copiedArray = [...hobbiesArray, "hello"]
+console.log(copiedArray);
+
+// rest operator
+// ... same as the spread operator but differnt in context
+const toArray = (...args) => {
+    return args;
+}
+
+console.log(toArray(1, 2, 3, 4));
