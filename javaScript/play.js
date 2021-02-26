a312d8eab9eb4b6474108c66170844766580724a
hopedorkenoo <compounddork@gmail.com>
2021-02-26 15:07:16 +0000

    JavaScript: Arrow Functions

--- javaScript/play.js
+++ javaScript/play.js
@@ -2,8 +2,18 @@ const name = "Julio";
 let age = 29;
 const hasHoddies = true;
 
-function summarizeUser(userName, userAge, userHasHobbies) {
+
+const summarizeUser = (userName, userAge, userHasHobbies) => {
     return "Name is " + userName + ",age is " + userAge + ' and the user has Hobbies' + userHasHobbies;
-}
+};
+
+const add = (a, b) => a + b;
+
+
+// function summarizeUser(userName, userAge, userHasHobbies) {
+//     return "Name is " + userName + ",age is " + userAge + ' and the user has Hobbies' + userHasHobbies;
+// }
+
+console.log(add(1, 3));
 
 console.log(summarizeUser(name, age, hasHoddies));
