866232ca9c895024a0b456e4d656ab91ec346ae0
hopedorkenoo <compounddork@gmail.com>
2021-02-27 12:07:01 +0000

    Node: Work with url and redirects

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // console.log("end");
-    // process.exit();
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My Firs Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.text', "dummy");
+        console.log(res.message);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -12,6 +27,8 @@ const server = http.createServer((req, res) => {
     res.write('<body><h1>Hello from the Node.js Server</h1><body>');
     res.write('</html>');
     res.end();
+    // console.log("end");
+    // process.exit();
 });
 
 server.listen(3000);
