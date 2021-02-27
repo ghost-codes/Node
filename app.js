97a65ef4e8661ac64bedb33a6715fba4a3a82613
hopedorkenoo <compounddork@gmail.com>
2021-02-27 11:54:31 +0000

    Node: Sending Response

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // console.log("end");
     // process.exit();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Firs Page</title></head>');
+    res.write('<body><h1>Hello from the Node.js Server</h1><body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
